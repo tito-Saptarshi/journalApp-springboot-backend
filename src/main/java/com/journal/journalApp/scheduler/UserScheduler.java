d6587819ddf60eadab7a236418e6b0e77b50a9e1
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-04-06 19:23:43 +0530

    general -u breakdown

---
+++ src/main/java/com/journal/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,42 @@
+package com.journal.journalApp.scheduler;
+
+import com.journal.journalApp.entity.JournalEntry;
+import com.journal.journalApp.entity.User;
+import com.journal.journalApp.repository.UserRepositoryImpl;
+import com.journal.journalApp.service.EmailService;
+import com.journal.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+// email functionalities used for sending emails
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+// finding users to sent the emails
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+//    @Scheduled(cron = "0 0/1 * 1/1 * ? *")
+    public void fetchUsersAndSendSaMail() {
+        List<User> users = userRepository.getUserForSA();
+        for (User user : users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "sentiment for last 7 days", "sentiment checker " + sentiment);
+        }
+    }
+}
--- src/test/java/com/journal/journalApp/JournalAppApplicationTests.java
+++ src/test/java/com/journal/journalApp/JournalAppApplicationTests.java
@@ -1,13 +1,15 @@
 package com.journal.journalApp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JournalAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Disabled
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
