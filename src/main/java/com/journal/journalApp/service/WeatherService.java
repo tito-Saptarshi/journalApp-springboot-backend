50d7f94bd6445a5c63c5b3baaf1513ca848a5ff1
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-04-09 02:47:39 +0530

    redis local -c

--- src/main/java/com/journal/journalApp/service/WeatherService.java
+++ src/main/java/com/journal/journalApp/service/WeatherService.java
@@ -23,19 +23,32 @@ public class WeatherService {
     @Autowired
     private AppCache appCache;
 
+    @Autowired
+    private RedisService redisService;
+
 //    private static final String apiKey = "4b53a35d8110d64e1e49932f42491a5f";
 
     public WeatherResponse getWeather (String city){
-//        String finalApi = appCache.APP_CACHE.replace("CITY", city).replace("API_KEY", apiKey);
-        String finalApi = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
+        WeatherResponse weatherResponse = redisService.get("weather_of_"+city, WeatherResponse.class);
+
+        if (weatherResponse != null) {
+            return weatherResponse;
+        } else {
+            //        String finalApi = appCache.APP_CACHE.replace("CITY", city).replace("API_KEY", apiKey);
+            String finalApi = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
+            ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
 //        above null represents header (currently has no header).
 //        restTemplate -> code to hit-request like an API
 
 //        json to java obj = deserialization
 
-        WeatherResponse body = response.getBody();
-        return body;
+            WeatherResponse body = response.getBody();
+            if (body != null) {
+                redisService.set("weather_of_" + city, body, 300l);
+            }
+            return body;
+        }
+
 
     }
 }
